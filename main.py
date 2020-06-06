fced8a36125a4bd5e03ec3ae017ffdf9b310595b
Malo Anceaux <malo.anceaux@mines-paristech.fr>
2020-06-06 13:55:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,28 +18,30 @@ from threshold import img_threshold
 from threshold import find_threshold
 from circles_detection import hough_circles
 from canny_filter import canny_threshold
+from cupules_detection import detection_cup
 
 #accessing path
-path = r'C:\Users\PULSAT\Documents\Mines\Transversalite\Info\Cupules\projet_cupules\img_png\TSP410.png'
+path = r'C:\Users\Malo Anceaux\Documents\Cours Mines Paristech\1 A\S2\Projet info\projet_cupules\img_png\TSP410.png'
 
 try :
 #here goes the main code
 
     img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     if img is None : raise ValueError
-    
+
     th = find_threshold(img)
 
     img_clean = img_threshold(img, th, 6)
     img_canny = canny_threshold(img_clean, 50, 100)
-    
+
     #img_circles = hough_circles(img_canny, param1=100, param2=30, minRadius=0, maxRadius=300)
-    
+    L = detection_cup(img)
     #affichage dans la fenetre
-    
+
     cv2.imshow("img canny", img_canny)
     cv2.imshow("img clean", img_clean)
     cv2.waitKey(0)
-    
+
+
 except ValueError as er :
     print(f"no image corresponding to path {path}")
