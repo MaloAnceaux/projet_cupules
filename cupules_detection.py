006c859855e7fe91e4aaf8a5c3b825626ff2e8c1
[Léo SIMPLET] <[leo.simplet@mines-paristech.fr]>
2020-06-06 21:01:52 +0200

    ajustements detection cupule

--- cupules_detection.py
+++ cupules_detection.py
@@ -2,10 +2,11 @@ def detection_cup(img):
     liste_cupules = []
     hauteur = len(img)
     largeur = len(img[0])
-    for i, j in (hauteur, largeur):
-        if img[i][j] == 0:
-            liste_cupules.append(parcours_int_cupules(img, i, j))
-    return liste_cupules
+    for i in range(hauteur):
+        for j in range(largeur):
+            if img[i][j] == 0:
+                liste_cupules.append(parcours_int_cupules(img, i, j))
+    return(liste_cupules)
 
 
 def parcours_int_cupules(img, i, j):
@@ -14,7 +15,7 @@ def parcours_int_cupules(img, i, j):
     cupule_a_faire = [(i, j)]
     while len(cupule_a_faire) > 0:
         n, p = cupule_a_faire[0]
-        cupule += parcours_rec(img, k, l)
+        cupule += (n, p)
         img[n][p] = 128
         del cupule_a_faire[0]
         if img[n + 1][p] == 0:
@@ -25,3 +26,4 @@ def parcours_int_cupules(img, i, j):
             cupule_a_faire.append((n, p + 1))
         if img[n][p - 1] == 0:
             cupule_a_faire.append((n, p - 1))
+    return(cupule)
